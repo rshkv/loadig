957b7513da58f1bd6745ed009e475e38db28aae9
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2015-12-18 21:04:23 +0100

    pip setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='loadig',
+    packages=['loadig'],
+    version='0.1',
+    description='A minimal progress bar.',
+    author='Willi Raschkowski',
+    author_email='willi.raschkowski@student.hpi.de',
+    url='https://github.com/busfahren/loadig',
+    download_url='https://github.com/busfahren/loadig/tarball/0.1',
+    keywords=['bar', 'loading', 'progress', 'logging'],
+    classifiers=[]
+)
