c9fa19168b67a9a020a985dfb59dccdc92798564
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2015-12-18 21:48:44 +0100

    setup.py to version 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='loadig',
     packages=['loadig'],
-    version='0.1',
+    version='0.2',
     description='A minimal progress bar.',
     author='Willi Raschkowski',
     author_email='willi.raschkowski@student.hpi.de',
     url='https://github.com/busfahren/loadig',
-    download_url='https://github.com/busfahren/loadig/tarball/0.1',
+    download_url='https://github.com/busfahren/loadig/tarball/0.2g',
     keywords=['bar', 'loading', 'progress', 'logging'],
     classifiers=[]
 )
