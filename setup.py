74d2ae111c57fb0aa2aeec84aebd9107165831c8
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2016-01-28 16:44:06 +0100

    fixed zero division error

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='loadig',
     packages=['loadig'],
-    version='0.5.1',
+    version='0.5.2',
     description='A minimal progress bar.',
     author='Willi Raschkowski',
     author_email='willi.raschkowski@student.hpi.de',
     url='https://github.com/busfahren/loadig',
-    download_url='https://github.com/busfahren/loadig/tarball/0.5.1',
+    download_url='https://github.com/busfahren/loadig/tarball/0.5.2',
     keywords=['bar', 'loading', 'progress', 'logging', 'loadig'],
     classifiers=[]
 )
